10ee0a619a7b4fab4d4e8e9f116a619d3233b247
d0whc3r <d0whc3r@gmail.com>
2020-05-23 19:42:37 +0200

    fix: add types for internal db functions

--- template/src/types/db.service-interface.ts
+++ template/src/types/db.service-interface.ts
@@ -1,6 +1,60 @@
 import moleculer, { Context } from 'moleculer';
 import { DbAdapter, DbContextParameters } from 'moleculer-db';
 
+export const listActionConfig = {
+  cache: {
+    keys: ['populate', 'fields', 'page', 'pageSize', 'sort', 'search', 'searchFields', 'query']
+  },
+  params: {
+    populate: [
+      { type: 'string', optional: true },
+      { type: 'array', optional: true, items: 'string' }
+    ],
+    fields: [
+      { type: 'string', optional: true },
+      { type: 'array', optional: true, items: 'string' }
+    ],
+    page: { type: 'number', integer: true, min: 1, optional: true, convert: true },
+    pageSize: { type: 'number', integer: true, min: 0, optional: true, convert: true },
+    sort: { type: 'string', optional: true },
+    search: { type: 'string', optional: true },
+    searchFields: [
+      { type: 'string', optional: true },
+      { type: 'array', optional: true, items: 'string' }
+    ],
+    query: [
+      { type: 'object', optional: true },
+      { type: 'string', optional: true }
+    ]
+  }
+};
+
+export const getActionConfig = {
+  cache: {
+    keys: ['id', 'populate', 'fields', 'mapping']
+  },
+  params: {
+    id: [{ type: 'string' }, { type: 'number' }, { type: 'array' }],
+    populate: [
+      { type: 'string', optional: true },
+      { type: 'array', optional: true, items: 'string' }
+    ],
+    fields: [
+      { type: 'string', optional: true },
+      { type: 'array', optional: true, items: 'string' }
+    ],
+    mapping: { type: 'boolean', optional: true }
+  }
+};
+
+export interface DBPagination<T> {
+  rows: T;
+  total: number;
+  page: number;
+  pageSize: number;
+  totalPages: number;
+}
+
 export class MoleculerDBService<T, R> extends moleculer.Service<T> {
   metadata!: {
     $category: string;
@@ -35,7 +89,7 @@ export class MoleculerDBService<T, R> extends moleculer.Service<T> {
    * @param {Boolean} decoding - Need to decode IDs.
    * @returns {Object|Array<Object>} Found entity(ies).
    */
-  getById!: <R>(id: string | number | string[], decoding?: boolean) => Promise<R>;
+  getById!: (id: string | number | string[], decoding?: boolean) => Promise<R>;
 
   /**
    * Clear the cache & call entity lifecycle events
@@ -45,7 +99,7 @@ export class MoleculerDBService<T, R> extends moleculer.Service<T> {
    * @param {Context} ctx
    * @returns {Promise}
    */
-  entityChanged!: <R>(type: string, json: number | any[] | any, ctx: Context) => Promise<R>;
+  entityChanged!: (type: string, json: number | any[] | any, ctx: Context) => Promise<R>;
 
   /**
    * Clear cached entities
@@ -62,7 +116,7 @@ export class MoleculerDBService<T, R> extends moleculer.Service<T> {
    * @param {Object}      Params
    * @returns {Array|Object}
    */
-  transformDocuments!: <R>(ctx: Context, params: any, docs: any) => R;
+  transformDocuments!: (ctx: Context, params: any, docs: any) => Promise<R | R[]>;
 
   /**
    * Filter fields in the entity object
@@ -71,7 +125,7 @@ export class MoleculerDBService<T, R> extends moleculer.Service<T> {
    * @param {Array}  fields  Filter properties of model.
    * @returns  {Object}
    */
-  filterFields!: <R>(doc: any, fields: any[]) => R;
+  filterFields!: (doc: any, fields: Partial<R>[]) => Partial<R>[];
 
   /**
    * Authorize the required field list. Remove fields which is not exist in the `this.settings.fields`
@@ -79,7 +133,7 @@ export class MoleculerDBService<T, R> extends moleculer.Service<T> {
    * @param {Array} fields
    * @returns {Array}
    */
-  authorizeFields!: <R>(fields: any[]) => R[];
+  authorizeFields!: (fields: Partial<R>[]) => Partial<R>[];
 
   /**
    * Populate documents.
@@ -131,4 +185,104 @@ export class MoleculerDBService<T, R> extends moleculer.Service<T> {
    * Service created lifecycle event handler
    */
   // created!: () => Promise<void>;
+
+  /**
+   * Find entities by query.
+   *
+   * @methods
+   *
+   * @param {Context} ctx - Context instance.
+   * @param {Object?} params - Parameters.
+   *
+   * @returns {Array<Object>} List of found entities.
+   */
+  _find!: (ctx: Context, params: any) => Promise<R[]>;
+
+  /**
+   * Get count of entities by query.
+   *
+   * @methods
+   *
+   * @param {Context} ctx - Context instance.
+   * @param {Object?} params - Parameters.
+   *
+   * @returns {Number} Count of found entities.
+   */
+  _count!: (ctx: Context, params: any) => Promise<number>;
+
+  /**
+   * List entities by filters and pagination results.
+   *
+   * @methods
+   *
+   * @param {Context} ctx - Context instance.
+   * @param {Object?} params - Parameters.
+   *
+   * @returns {Object} List of found entities and count.
+   */
+  _list!: (ctx: Context, params: any) => Promise<DBPagination<R>>;
+
+  /**
+   * Create a new entity.
+   *
+   * @methods
+   *
+   * @param {Context} ctx - Context instance.
+   * @param {Object?} params - Parameters.
+   *
+   * @returns {Object} Saved entity.
+   */
+  _create!: (ctx: Context, params: any) => Promise<R>;
+
+  /**
+   * Create many new entities.
+   *
+   * @methods
+   *
+   * @param {Context} ctx - Context instance.
+   * @param {Object?} params - Parameters.
+   *
+   * @returns {Object|Array.<Object>} Saved entity(ies).
+   */
+  _insert!: (ctx: Context, params: any) => Promise<R | R[]>;
+
+  /**
+   * Get entity by ID.
+   *
+   * @methods
+   *
+   * @param {Context} ctx - Context instance.
+   * @param {Object?} params - Parameters.
+   *
+   * @returns {Object|Array<Object>} Found entity(ies).
+   *
+   * @throws {EntityNotFoundError} - 404 Entity not found
+   */
+  _get!: (ctx: Context, params: any) => Promise<R | R[]>;
+
+  /**
+   * Update an entity by ID.
+   * > After update, clear the cache & call lifecycle events.
+   *
+   * @methods
+   *
+   * @param {Context} ctx - Context instance.
+   * @param {Object?} params - Parameters.
+   * @returns {Object} Updated entity.
+   *
+   * @throws {EntityNotFoundError} - 404 Entity not found
+   */
+  _update!: (ctx: Context, params: any) => Promise<R>;
+
+  /**
+   * Remove an entity by ID.
+   *
+   * @methods
+   *
+   * @param {Context} ctx - Context instance.
+   * @param {Object?} params - Parameters.
+   *
+   * @throws {EntityNotFoundError} - 404 Entity not found
+   */
+  _remove!: (ctx: Context, params: any) => Promise<void>;
 }
